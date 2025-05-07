1a9f7760bfd78fe6e1b2db5fb885f0c5146c51e3
brandon kim <brandonkim12904@gmail.com>
2025-05-07 15:30:20 -0400

    Some data displayed - button display up next

---
+++ src/app/_components/addCharacterButton.tsx
@@ -0,0 +1,10 @@
+import React, { useState} from 'react'
+import { api } from "~/trpc/react";
+
+const addCharacterButton = () => {
+    
+
+    return <button></button>;
+}
+
+export default addCharacterButton;
--- src/app/_components/post.tsx
+++ src/app/_components/post.tsx
@@ -1,5 +1,30 @@
 "use client";
 
 import { useState } from "react";
-
+import { PrismaClient } from "@prisma/client";
 import { api } from "~/trpc/react";
+
+const PlayerData = () => {
+    const { data: users, isLoading } = api.playerCharacter.getAll.useQuery()
+
+    return (
+        <table> 
+            <thead>
+                <tr>
+                    <th>
+                        <ul>
+                        {users?.map((user) => (
+                            <li key={user.id}>
+                                {user.name} - {user.email}
+                            </li>
+                            ))}
+                        </ul>
+                    </th>    
+                </tr>
+            </thead>
+        </table>
+        
+    )
+}
+
+export default PlayerData
