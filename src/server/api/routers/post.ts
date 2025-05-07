9ced3bfb86edb5a892332afc3b6666cbcdefad6e
brandon kim <brandonkim12904@gmail.com>
2025-05-07 10:09:22 -0400

    Removed Post capabilities
    Post queries and database has been removed to be eventually replaced with player character data.

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -15,17 +15,6 @@ export const postRouter = createTRPCRouter({
       };
     }),
 
-  create: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      return ctx.db.post.create({
-        data: {
-          name: input.name,
-          createdBy: { connect: { id: ctx.session.user.id } },
-        },
-      });
-    }),
-
   getLatest: protectedProcedure.query(async ({ ctx }) => {
     const post = await ctx.db.post.findFirst({
       orderBy: { createdAt: "desc" },
